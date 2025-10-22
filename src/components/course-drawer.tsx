5daa2d2abd83adcbf3ceacb311712907837a2ed2
William Dai <william.3dai@gmail.com>
2025-10-22 01:52:58 -0400

    added requisite disclaimer

--- src/components/course-drawer.tsx
+++ src/components/course-drawer.tsx
@@ -114,6 +114,25 @@ export function CourseDrawer({ course, onClose, onAddToPlan: _onAddToPlan }: Cou
               </CardContent>
             </Card>
 
+            {/* Disclaimer Box */}
+            <div className="bg-amber-50 border border-amber-200 rounded-lg p-4 mt-4">
+              <div className="flex items-start">
+                <div className="flex-shrink-0">
+                  <svg className="h-5 w-5 text-amber-400" viewBox="0 0 20 20" fill="currentColor">
+                    <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                  </svg>
+                </div>
+                <div className="ml-3">
+                  <h4 className="text-sm font-medium text-amber-800">Important Notice</h4>
+                  <p className="text-sm text-amber-700 mt-1">
+                    Course requirements and information may be outdated. Please consult official University of Waterloo resources 
+                    (such as the undergraduate calendar, course catalog, or academic advisors) for the most current and accurate 
+                    course requirements and information.
+                  </p>
+                </div>
+              </div>
+            </div>
+
             {/* Prerequisites */}
             {course.prerequisites.length > 0 && (
               <Card className="border-red-200 bg-red-50">
